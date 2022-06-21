ce0b0f5b6ef56cb5ce3090333ec6b2b85721085e
shgur7236 <shgurtns7236@naver.com>
2022-06-21 21:07:13 +0900

    Repository interface 메서드 실습

--- src/main/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.fastcampus.jpa.bookmanager.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
 
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.fastcampus.jpa.bookmanager.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,7 +19,12 @@ class UserRepositoryTest {
 
     @Test
     void crud() { // Create,Read,Update,Delete
-        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+        User user1 = new User("jack", "jack@fastcampus.com");
+        User user2 = new User("steve", "steve@fastcampus.com");
+
+        userRepository.saveAll(Lists.newArrayList(user1, user2));
+
+        List<User> users = userRepository.findAll();
 
         users.forEach(System.out::println);
     }
