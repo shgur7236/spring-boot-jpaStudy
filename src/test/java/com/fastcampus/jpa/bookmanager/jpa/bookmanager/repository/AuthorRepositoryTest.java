1f5687f97c2b657a895e1792e63f4d2222918cf1
shgur7236 <shgurtns7236@naver.com>
2022-07-18 23:43:57 +0900

    n:n

--- src/test/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/AuthorRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/repository/AuthorRepositoryTest.java
@@ -2,6 +2,7 @@ package com.fastcampus.jpa.bookmanager.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.Author;
 import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.Book;
+import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.BookAndAuthor;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class AuthorRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private BookAndAuthorRepository bookAndAuthorRepository;
+
+
     @Test
     @Transactional
     void manyToManyTest(){
@@ -29,19 +34,36 @@ public class AuthorRepositoryTest {
         Author author1 = givenAuthor("노혁");
         Author author2 = givenAuthor("곽희상");
 
-        book1.addAuthor(author1);
-        book2.addAuthor(author2);
-        book3.addAuthor(author1,author2);
-        book4.addAuthor(author1,author2);
+//        book1.addAuthor(author1);
+//        book2.addAuthor(author2);
+//        book3.addAuthor(author1,author2);
+//        book4.addAuthor(author1,author2);
+//
+//        author1.addBook(book1,book3,book4);
+//        author2.addBook(book2,book3,book4);
 
-        author1.addBook(book1,book3,book4);
-        author2.addBook(book2,book3,book4);
+        BookAndAuthor bookAndAuthor1 = givenBookAndAuthor(book1, author1);
+        BookAndAuthor bookAndAuthor2 = givenBookAndAuthor(book2, author2);
+        BookAndAuthor bookAndAuthor3 = givenBookAndAuthor(book3, author1);
+        BookAndAuthor bookAndAuthor4 = givenBookAndAuthor(book3, author2);
+        BookAndAuthor bookAndAuthor5 = givenBookAndAuthor(book4, author1);
+        BookAndAuthor bookAndAuthor6 = givenBookAndAuthor(book4, author2);
+
+        book1.addBookAndAuthors(bookAndAuthor1);
+        book2.addBookAndAuthors(bookAndAuthor2);
+        book3.addBookAndAuthors(bookAndAuthor3, bookAndAuthor4);
+        book4.addBookAndAuthors(bookAndAuthor5, bookAndAuthor6);
+
+        author1.addBookAndAuthors(bookAndAuthor1, bookAndAuthor3, bookAndAuthor5);
+        author1.addBookAndAuthors(bookAndAuthor2, bookAndAuthor4, bookAndAuthor6);
 
         bookRepository.saveAll(Lists.newArrayList(book1,book2,book3,book4));
         authorRepository.saveAll(Lists.newArrayList(author1,author2));
 
-        System.out.println("authors through book : " + bookRepository.findAll().get(2).getAuthors());
-        System.out.println("books through author : " + authorRepository.findAll().get(0).getBooks());
+//        System.out.println("authors through book : " + bookRepository.findAll().get(2).getAuthors());
+//        System.out.println("books through author : " + authorRepository.findAll().get(0).getBooks());
+        bookRepository.findAll().get(2).getBookAndAuthors().forEach(o -> System.out.println(o.getAuthor()));
+        authorRepository.findAll().get(0).getBookAndAuthors().forEach(o -> System.out.println(o.getBook()));
 
     }
 
@@ -58,5 +80,14 @@ public class AuthorRepositoryTest {
 
         return authorRepository.save(author);
     }
+
+    private BookAndAuthor givenBookAndAuthor(Book book, Author author){
+        BookAndAuthor bookAndAuthor = new BookAndAuthor();
+        bookAndAuthor.setAuthor(author);
+        bookAndAuthor.setBook(book);
+
+        return bookAndAuthorRepository.save(bookAndAuthor);
+    }
+
 
 }
