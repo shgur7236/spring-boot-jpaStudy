480062ab33e765798c0ecbecc22578e44cbbbae8
shgur7236 <shgurtns7236@naver.com>
2022-07-22 15:16:03 +0900

    영속성 상태

--- src/main/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/service/UserService.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/jpa/bookmanager/service/UserService.java
@@ -1,16 +1,29 @@
 package com.fastcampus.jpa.bookmanager.jpa.bookmanager.service;
 
 import com.fastcampus.jpa.bookmanager.jpa.bookmanager.domain.User;
+import com.fastcampus.jpa.bookmanager.jpa.bookmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 @Service
 public class UserService {
+    @Autowired
+    private EntityManager entityManager;
+
+//    @Autowired
+//    private UserRepository userRepository;
+
     @Transactional
     public void put(){
         User user = new User();
         user.setName("newUser");
         user.setEmail("newUser@fastcampus.com");
 
+//        userRepository.save(user);
+
+        entityManager.persist(user);
     }
 }
